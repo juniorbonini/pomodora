80414303e6e651ecec340a361aec783b78107bc3
juniorbonini <juniorboninia7x@gmail.com>
2026-02-09 15:32:13 -0300

    feat: Menu componente criado.

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,21 @@
+import { History, House, Settings, Sun } from "lucide-react";
+import styles from "./style.module.css";
+
+export const Menu = () => {
+  return (
+    <nav className={styles.container}>
+      <a href="" className={styles.link}>
+        <House />
+      </a>
+      <a href="" className={styles.link}>
+        <History />
+      </a>
+      <a href="" className={styles.link}>
+        <Settings />
+      </a>
+      <a href="" className={styles.link}>
+        <Sun />
+      </a>
+    </nav>
+  );
+};
