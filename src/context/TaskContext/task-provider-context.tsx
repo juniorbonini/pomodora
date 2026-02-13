d59c3b7baea750f59069c880e14ea35918e60632
juniorbonini <juniorboninia7x@gmail.com>
2026-02-13 12:52:05 -0300

    feat: atualizar controle do timer e adicionar áudio ao finalizar ciclo
    - Atualiza as props secondsRemaining e formattedSecondsRemaining
    - Ajusta componentes que utilizam esses estados
    - Cria utilitário para reprodução de áudio ao concluir uma task
    - Integra a execução do áudio ao provider para disparar automaticamente ao finalizar o ciclo

--- src/context/TaskContext/task-provider-context.tsx
+++ src/context/TaskContext/task-provider-context.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useReducer } from "react";
+/* eslint-disable react-hooks/refs */
+import { useEffect, useReducer, useRef } from "react";
 
 import { TaskContext } from "./task-context";
 import { taskInitialState } from "@/models/Task/task-initial-state";
@@ -6,15 +7,21 @@ import type { TaskContextProviderProps } from "@/models/Task/task-context";
 import { taskReducer } from "@/context/TaskReducer/task-reducer";
 import { TimerWorkerManager } from "@/workers/timer-worker-manager";
 import { TaskActionTypes } from "../TaskReducer/task-aciton";
+import { loadBeep } from "@/utils/Audios/load-beep";
 
 export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
   const [task, setTask] = useReducer(taskReducer, taskInitialState);
   const worker = TimerWorkerManager.getInstance();
+  const playBeepAudio = useRef<ReturnType<typeof loadBeep> | null>(null);
 
   worker.onmessage((e) => {
     const countDownSeconds = e.data;
     console.log(typeof countDownSeconds);
     if (countDownSeconds <= 0) {
+      if (playBeepAudio.current) {
+        playBeepAudio.current();
+        playBeepAudio.current = null;
+      }
       setTask({ type: TaskActionTypes.COMPLETE_TASK });
       worker.terminate();
     } else {
@@ -33,6 +40,14 @@ export const TaskContextProvider = ({ children }: TaskContextProviderProps) => {
 
     worker.postMessage(task);
   }, [task, worker]);
+
+  useEffect(() => {
+    if (task.activeTask && playBeepAudio.current === null) {
+      playBeepAudio.current = loadBeep();
+    } else {
+      playBeepAudio.current = null;
+    }
+  }, [task.activeTask]);
   return (
     <TaskContext.Provider
       value={{
---
+++ src/utils/Audios/load-beep.ts
@@ -0,0 +1,11 @@
+import audioBeep from "@/assets/audios/gravitational_beep.mp3";
+
+export function loadBeep() {
+  const audio = new Audio(audioBeep);
+  audio.load();
+
+  return () => {
+    audio.currentTime = 0;
+    audio.play().catch((error) => console.log("Erro ao tocar áudio", error));
+  };
+}
